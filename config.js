c500a0daf80262d5aa1a92502b79783edbadd999
bingbing <bingbing01121@gmail.com>
2026-01-13 17:46:44 +0700

    docs: Add comments to config.js to explain its purpose

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 // config.js
+// 该文件用于配置 WebRTC 的 ICE 服务器。
+// ICE (Interactive Connectivity Establishment) 框架使用这些服务器来帮助建立对等连接，
+// 特别是在复杂的网络环境（如NAT或防火墙）中。
+
 module.exports = {
     iceServers: [
      // 请替换为您的TURN服务器地址、用户名和密码
