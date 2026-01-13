b0b7e939fdad3b53075f72c1e76830dcbcdb6906
bingbing <bingbing01121@gmail.com>
2026-01-13 15:33:36 +0700

    增加turn服务

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 // config.js
 module.exports = {
     iceServers: [
+     // 请替换为您的TURN服务器地址、用户名和密码
+           {
+                             'urls': 'turn:free.expressturn.com:3478',
+                             'username': '000000002083647736',
+                             'credential': '4rrveXxVAGyhGl7lorJasxF1aZ4='
+           },
         {
             urls: 'stun:stun.l.google.com:19302'
-        },
-        // 请替换为您的TURN服务器地址、用户名和密码
-        {
-                  'urls': 'turn:free.expressturn.com:3478',
-                  'username': '000000002083647736',
-                  'credential': '4rrveXxVAGyhGl7lorJasxF1aZ4='
-        },
-        
+        }
     ]
 };
